1e9b683b7c28f8720eb2d5eab231cd2bab24e3da
ThetaPilla <thetapilla@gmail.com>
2022-11-20 16:38:58 +0800

    update param preprocessing utils

--- nonebot_plugin_colab_novelai/utils/__init__.py
+++ nonebot_plugin_colab_novelai/utils/__init__.py
@@ -6,5 +6,5 @@ from .distributed import (
     PLUGIN_DIR, NSFW_TAGS,
     T_UserID, T_AuthorizedUserID, T_GroupID, T_AuthorizedGroupID,
     get_mac_address, convert_audio2wav,
-    fetch_image_in_message, inject_image_to_state
+    fetch_image_in_message, inject_image_to_state, preprocess_painting_parameters
 )
--- nonebot_plugin_colab_novelai/utils/distributed.py
+++ nonebot_plugin_colab_novelai/utils/distributed.py
@@ -1,7 +1,10 @@
+import re
 import os
 import uuid
+import random
 from pathlib import Path
-from typing import Any, Type, TypeVar, Optional, Union
+from argparse import Namespace
+from typing import Tuple, Dict, Any, Type, TypeVar, Optional, Union
 
 import av
 
@@ -82,3 +85,46 @@ async def inject_image_to_state(
     if (image := await fetch_image_in_message(image_arg)) is None:
         await matcher.finish("格式错误，请重新触发指令..")
     state["baseimage"] = image
+
+
+def nsfw_tag_filter(prompt: str, uc: str, on: bool = True) -> Tuple[str, str]:
+    if on:
+        uc += ','.join(NSFW_TAGS)
+
+        prompts = set(prompt.split(','))
+        for p in prompts.copy():
+            for nsfw_tag in NSFW_TAGS:
+                if re.match(nsfw_tag, p, flags=re.I) is not None:
+                    prompts.discard(p)
+        if not prompts:
+            raise ValueError("色鬼，好好检查一下你都放了些什么tag进来！")
+        prompt = ','.join(prompts)
+
+    return prompt, uc
+
+
+async def preprocess_painting_parameters(matcher: Type[Matcher], args: Namespace, on_nsfw: bool) -> Dict[str, Any]:
+    try:
+        assert 1 <= args.num <= plugin_config.naifu_max, "图片数量超过上限！"
+        assert -1 <= args.seed <= 2**32 - 1, "设置的种子需要在-1与2^32-1之间！"
+        assert 1 <= args.steps <= 50, "设置的steps需要在1到50之间！"
+        assert 1.1 <= args.scale <= 100, "设置的scale需要在1.1到100之间！"
+    except AssertionError as e:
+        await matcher.finish(str(e), at_sender=True)
+
+    prompts, uc = nsfw_tag_filter(
+        prompt="masterpiece,best quality," + ' '.join(args.prompt).replace('，', ','),
+        uc="lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, "
+           "worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry,"
+           + ' '.join(args.undesired_content).replace('，', ',').strip(',') + ',',
+        on=on_nsfw
+    )
+    return {
+        "prompt": prompts,
+        "uc": uc,
+        "n_samples": args.num,
+        "seed": random.randint(0, 2**32 - 1) if args.seed == -1 else args.seed,
+        "sampler": args.sampling,
+        "steps": args.steps,
+        "scale": round(args.scale, 1)
+    }
